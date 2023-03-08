1e5e78dc88d3f994c0e327c239b6b0657a39e380
MaxatDzhetenbayev <yaboku18@mail.ru>
2023-03-08 16:51:37 +0600

    extended jwt functional with jwt-strategy and jwt-guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,8 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getJwtConfig } from 'src/configs/jwt.config';
+import { PassportModule } from '@nestjs/passport'
+import { JwtStrategy } from 'src/strategies/jwt.strategy';
 
 @Module({
 	controllers: [AuthController],
@@ -18,14 +20,14 @@ import { getJwtConfig } from 'src/configs/jwt.config';
 				}
 			}
 		]),
+		ConfigModule,
 		JwtModule.registerAsync({
 			imports: [ConfigModule],
 			inject: [ConfigService],
 			useFactory: getJwtConfig
-		})
-
+		}),
+		PassportModule
 	],
-
-	providers: [AuthService]
+	providers: [AuthService, JwtStrategy]
 })
 export class AuthModule { }
---
+++ src/guards/jwt.guard.ts
@@ -0,0 +1,4 @@
+import { AuthGuard } from '@nestjs/passport'
+
+
+export class JwtGuard extends AuthGuard('jwt') { }
