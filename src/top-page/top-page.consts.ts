e541866ea0fc516662b16ef4fbead612ac966da4
MaxatDzhetenbayev <yaboku18@mail.ru>
2023-03-14 10:16:30 +0600

    created new top-page.services and his functional

---
+++ src/top-page/top-page.consts.ts
@@ -0,0 +1,2 @@
+export const TOP_PAGE_NOT_FOUND_ERROR = 'страница с таким id не была найдена'
+export const TOP_PAGE_BY_CATEGORY_ERROR = 'страница с данной категорией не была найдена'
---
+++ src/top-page/top-page.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { DocumentType, ModelType } from '@typegoose/typegoose/lib/types';
+import { InjectModel } from 'nestjs-typegoose';
+import { CreatePageDto } from './dto/create-page.dto';
+import { FindTopPageDto } from './dto/find-top-page.dto';
+import { TopLevelCategory, TopPageModel } from './top-page.model';
+
+@Injectable()
+export class TopPageService {
+	constructor(@InjectModel(TopPageModel) private readonly pageModel: ModelType<TopPageModel>) { }
+
+	async create(dto: CreatePageDto): Promise<DocumentType<TopPageModel>> {
+		return this.pageModel.create(dto)
+	}
+
+	async findPageById(id: Pick<TopPageModel, '_id'>): Promise<DocumentType<TopPageModel> | null> {
+		return this.pageModel.findById(id).exec()
+	}
+
+	async findPageByAlias(alias: Pick<TopPageModel, 'alias'>): Promise<DocumentType<TopPageModel> | null> {
+		return this.pageModel.findOne({ alias }).exec()
+	}
+
+	async deleteById(id: Pick<TopPageModel, '_id'>) {
+		return this.pageModel.deleteOne({ _id: id }).exec()
+	}
+
+	async updatePageById(id: Pick<TopPageModel, '_id'>, dto: TopPageModel) {
+		return this.pageModel.findByIdAndUpdate(id, dto, { new: true }).exec()
+	}
+
+	async findByCategory(firstLevelCategory: TopLevelCategory) {
+		return this.pageModel.find({ firstLevelCategory }, { alias: 1, title: 1, secondLevelCategory: 1 }).exec()
+	}
+}
